19ed77f9f5bf0e338a176d24053027c7430f70e7
Jeremie Gillet <jie.gillet@gmail.com>
2023-12-12 11:59:38 +0900

    Create .pot without msgstr and complete en .po file

--- lib/countries_gettext/generate_files.ex
+++ lib/countries_gettext/generate_files.ex
@@ -14,24 +14,31 @@ defmodule CountriesGettext.GenerateFiles do
 
   def generate_pot(output_dir) do
     content =
-      @country_codes_input_json_path
-      |> File.read!()
-      |> Jason.decode!()
-      |> Map.get("3166-1")
+      @codes_by_name
       |> Enum.map(&pot_entry_for_country/1)
       |> Enum.join("\n")
 
+    File.mkdir_p!(output_dir)
     File.write!(pot_path(output_dir), content)
+
+    en_po_content =
+      @codes_by_name
+      |> Enum.map(&en_po_entry_for_country/1)
+      |> Enum.join("\n")
+
+    File.mkdir_p!(po_directory(output_dir, "en"))
+    File.write!(po_path(output_dir, "en"), en_po_content)
   end
 
   def generate_po(output_dir, locale) do
     input =
       locale
-      |> po_input_json_path
+      |> po_input_json_path()
       |> File.read!()
 
     content = Regex.replace(~r/msgid "([^"]+)"/, input, &replace_name_by_code/2)
 
+    File.mkdir_p!(po_directory(output_dir, locale))
     File.write!(po_path(output_dir, locale), content)
   end
 
@@ -42,9 +49,14 @@ defmodule CountriesGettext.GenerateFiles do
     end
   end
 
-  def pot_entry_for_country(%{"alpha_2" => code, "name" => name} = map) do
-    name = map["common_name"] || name
+  def pot_entry_for_country({_name, code}) do
+    ~s"""
+    msgid "#{String.downcase(code)}"
+    msgstr ""
+    """
+  end
 
+  def en_po_entry_for_country({name, code}) do
     ~s"""
     msgid "#{String.downcase(code)}"
     msgstr "#{name}"
@@ -55,13 +67,17 @@ defmodule CountriesGettext.GenerateFiles do
     Path.join([output_dir, "countries.pot"])
   end
 
+  defp po_directory(output_dir, locale) do
+    Path.join([output_dir, locale, "LC_MESSAGES"])
+  end
+
   defp po_path(output_dir, locale) do
     Path.join([output_dir, locale, "LC_MESSAGES", "countries.po"])
   end
 
   def po_input_json_path(locale) do
     :code.priv_dir(:countries_gettext)
-    |> to_string
+    |> to_string()
     |> Path.join(Path.join(["iso-codes", "iso_3166-1", "#{locale}.po"]))
   end
 end
